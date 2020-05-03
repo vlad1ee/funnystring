1974b89d764f1e902375f6af42d3003885024476
vladl01 <vladlee.2001@mail.ru>
2020-05-03 17:11:15 +0600

    add file

---
+++ funnystring.py
@@ -0,0 +1,11 @@
+# def funnyString(s):
+#     rvrs = s[::-1]
+#     for i in range(1, len(s)):
+#         s2 = (abs(ord(s[i]) - ord(s[i-1])))
+#     for e in range(1, len(rvrs)):
+#         s3 = (abs(ord(rvrs[e]) - ord(rvrs[e-1])))
+#     if s2 == s3:
+#         return('Funny')
+#     else:
+#         return('Not Funny')
+# print(funnyString('acxz'))
